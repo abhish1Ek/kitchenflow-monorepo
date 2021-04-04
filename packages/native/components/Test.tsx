4a3784c2be3df64be2cd66fa7fc41c6cc4085835
abhish1Ek <abhish1.mishra@gmail.com>
2021-04-04 16:29:39 +0530

    initialised monorepo, tested ios, web setup for restarantManager app

---
+++ packages/native/components/Test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Test: React.FC = (props) => {
+  return (
+    <View style={{ backgroundColor: "orange" }}>
+      <Text>Test component</Text>
+    </View>
+  );
+};
+
+export default Test;
---
+++ packages/restaurantManager/webpack.config.js
@@ -0,0 +1,100 @@
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+function getDirectories(path) {
+  return fs.readdirSync(path).filter(function (file) {
+    return fs.statSync(path + '/' + file).isDirectory();
+  });
+}
+
+const appDirectory = path.resolve(__dirname, '../../');
+const packages = getDirectories(path.resolve(appDirectory, './packages'));
+
+const currentDirectory = path.resolve(__dirname);
+const {presets} = require(`${currentDirectory}/babel.config.js`);
+
+const omitPackages = [];
+
+const compileNodeModules = [
+  // Add every react-native package that needs compiling
+  // 'react-native-gesture-handler',
+].map(moduleName => path.resolve(appDirectory, `./node_modules/${moduleName}`));
+
+const packagesToWatch = packages
+  .filter(package => !omitPackages.includes(package))
+  .map(package => path.resolve(appDirectory, `./packages/${package}`));
+
+const babelLoaderConfiguration = {
+  test: /\.js$|tsx?$/,
+  // Add every directory that needs to be compiled by Babel during the build.
+  include: [
+    path.resolve(__dirname, 'index.web.js'), // Entry to your application
+    path.resolve(__dirname, 'App.web.tsx'), // Change this to your main App file
+    path.resolve(__dirname, 'src'),
+    ...compileNodeModules,
+    ...packagesToWatch,
+  ],
+  use: {
+    loader: 'babel-loader',
+    options: {
+      cacheDirectory: true,
+      presets,
+      plugins: ['react-native-web'],
+    },
+  },
+};
+
+const svgLoaderConfiguration = {
+  test: /\.svg$/,
+  use: [
+    {
+      loader: '@svgr/webpack',
+    },
+  ],
+};
+
+const imageLoaderConfiguration = {
+  test: /\.(gif|jpe?g|png)$/,
+  use: {
+    loader: 'url-loader',
+    options: {
+      name: '[name].[ext]',
+    },
+  },
+};
+
+module.exports = {
+  entry: {
+    app: path.join(__dirname, 'index.web.js'),
+  },
+  output: {
+    path: path.resolve(currentDirectory, 'dist'),
+    publicPath: '/',
+    filename: 'rnw_kitchenflow_restaurantManager.bundle.js',
+  },
+  resolve: {
+    extensions: ['.web.tsx', '.web.ts', '.tsx', '.ts', '.web.js', '.js'],
+    alias: {
+      'react-native$': 'react-native-web',
+    },
+  },
+  module: {
+    rules: [
+      babelLoaderConfiguration,
+      imageLoaderConfiguration,
+      svgLoaderConfiguration,
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html'),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      // See: https://github.com/necolas/react-native-web/issues/349
+      __DEV__: JSON.stringify(true),
+    }),
+  ],
+};
