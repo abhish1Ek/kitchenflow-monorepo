293eb653ed23320c4a48b78b4564b19668bf8347
abhish1Ek <abhish1.mishra@gmail.com>
2021-04-09 17:03:20 +0530

    tested staffApp on ios and web

---
+++ packages/staffApp/src/Main.tsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import Test from 'native/components/Test';
+
+const Main = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Hel ssslo from {'\n'}React ss Native Web!
+      </Text>
+      <TouchableOpacity
+        onPress={() => setCount(count + 1)}
+        style={styles.button}>
+        <Text>Click me!</Text>
+      </TouchableOpacity>
+
+      <Text>You clicked {count} times!</Text>
+      <Test />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#C3E8BD',
+    paddingTop: 40,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: '#ADBDFF',
+    padding: 5,
+    marginVertical: 20,
+    alignSelf: 'flex-start',
+  },
+  title: {
+    fontSize: 40,
+  },
+});
+
+export default Main;
---
+++ packages/staffApp/webpack.config.js
@@ -0,0 +1,47 @@
+const {merge} = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const commonConfig = require('../../buildUtils/webpack.common.js');
+const createBabelLoaderConfig = require('../../buildUtils/createBabelLoaderConfig');
+
+const babelLoaderConfiguration = createBabelLoaderConfig({
+  omitPackages: [],
+
+  // Add every directory that needs to be compiled by Babel during the build.
+  include: [
+    path.resolve(__dirname, 'index.web.js'), // Entry to your application
+    path.resolve(__dirname, 'App.web.tsx'), // Change this to your main App file
+    path.resolve(__dirname, 'src'),
+  ],
+  compileNodeModules: [
+    // Add every react-native package that needs compiling
+    // 'react-native-gesture-handler',
+  ],
+});
+
+const config = {
+  entry: {
+    app: path.join(__dirname, 'index.web.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'rnw_kitchenflow_staffApp.bundle.js',
+  },
+
+  module: {
+    rules: [babelLoaderConfiguration],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html'),
+    }),
+  ],
+  devServer: {
+    port: 3001,
+  },
+};
+
+module.exports = ({env}) => {
+  return merge(commonConfig, config);
+};
